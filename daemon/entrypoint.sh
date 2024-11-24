cbe33d82d04ba63d6b03b8a34d0031566415774e
Daniel Leet <dmleet1@gmail.com>
2024-11-24 12:00:42 -0500

    Remove config injection script

--- daemon/entrypoint.sh
+++ daemon/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Injecting core config"
-python3 /opt/deluge/inject_core_config.py
-
 echo "Starting deluged"
 deluged --do-not-daemonize --config /home/deluge/.config/deluge --loglevel=${DELUGE_LOGLEVEL}
